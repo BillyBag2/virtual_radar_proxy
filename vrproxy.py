919c7b3a395bb9c8e48d0e6151e764383bbe4981
William Bagshaw <bagshawwilliam@gnail.com>
2021-05-29 15:05:11 +0000

    Kind of working

--- vrproxy.py
+++ vrproxy.py
@@ -1,18 +1,37 @@
 #!/bin/python3
 
-
 import socket
+import sys
 
-s = socket.create_connection(('51.38.80.19',33001))
 
-while(True):
-	data = s.recv(10)
-	print(data)
+HELP= sys.argv[0] + " <source address> <source port>"
+def  proxy(source_address,source_port):
+	s = socket.create_connection((source_address,source_port))
+	s.setblocking(0)
+	buffer = ""
+	while(True):
+		try:
+			data = s.recv(10000)
+			#print(data)
+			buffer = buffer + data.decode('utf-8')
+			if(data == b''):
+				print("Connction Closed")
+				break
+			lines = buffer.split("\r\n")
+			for line in lines:
+				print('>>>\n')
+				print(line)
+				print('<<<\r')
+			buffer = ""
+		except socket.error:
+        		pass
+	s.close()
 
-	if(data == b''):
-		print("Connction Closed")
-		break
 
-s.close()
+if __name__ == "__main__":
+	if not (len(sys.argv) == 3):
+		print(HELP)
+		sys.exit()
+	proxy(sys.argv[1],sys.argv[2])
 
 
