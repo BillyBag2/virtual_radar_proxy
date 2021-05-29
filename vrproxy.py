f9ebf4d270707f7acc0ee0c81a30da2ce5b3dc9d
William Bagshaw <bagshawwilliam@gnail.com>
2021-05-29 15:27:17 +0000

    Now handles lines correctly.

--- vrproxy.py
+++ vrproxy.py
@@ -5,26 +5,40 @@ import sys
 
 
 HELP= sys.argv[0] + " <source address> <source port>"
+
+
+def aline(line):
+	print(">>" + line + "<<")
+
+
 def  proxy(source_address,source_port):
 	s = socket.create_connection((source_address,source_port))
 	s.setblocking(0)
 	buffer = ""
 	while(True):
 		try:
-			data = s.recv(10000)
+			data = s.recv(200)
 			#print(data)
 			buffer = buffer + data.decode('utf-8')
 			if(data == b''):
 				print("Connction Closed")
 				break
-			lines = buffer.split("\r\n")
-			for line in lines:
-				print('>>>\n')
-				print(line)
-				print('<<<\r')
-			buffer = ""
+			lines = buffer.splitlines(True)
+			for line in lines[:-1]:
+				aline(line)
+			#print(len(lines))
+			last = lines[-1]
+			if last.endswith("\r\n"):
+				aline(last)
+				buffer = ""
+				#print("+++++++")
+			else:
+				# Last line is only partial.
+				buffer = lines[-1]
+				#print('************************' + buffer)
 		except socket.error:
-        		pass
+			#print('.')
+			pass
 	s.close()
 
 
