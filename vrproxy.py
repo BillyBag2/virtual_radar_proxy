2e27c2248b60dc791680053dfc1eaa9631902a98
William Bagshaw <bagshawwilliam@gnail.com>
2021-06-12 09:30:50 +0000

    Now filters out messages with no location information.

--- vrproxy.py
+++ vrproxy.py
@@ -13,20 +13,34 @@ HELP= sys.argv[0] + " <source address> <source port>"
 
 MSG_ID = 0
 MSG_TYPE = 1
+MSG_MODES = 4
 MSG_LAT = 14
 MSG_LONG = 15
+
+g_craft = {}
 
 def log(line):
 	parts = line.split(",")
 	if parts[MSG_ID] != "MSG":
 		print("Unknown message type", parts[MSG_TYPE])
-		return
+		return False
 		
 	msgType = int(parts[MSG_TYPE])
-	print(parts[MSG_ID],msgType)
+	msgModes = parts[MSG_MODES]
+	
+	#print(parts[MSG_ID],msgType)
 	if (msgType == 2) or (msgType == 3):
 		# This contains long and lat.
-		print(parts[MSG_LONG],",",parts[MSG_LAT])
+		# print(msgModes," : ", parts[MSG_LONG],",",parts[MSG_LAT])
+		g_craft[msgModes] = (parts[MSG_LONG],parts[MSG_LAT])
+		return True
+	
+	if g_craft.get(msgModes) != None:
+		return True
+		
+	print("No location for...",msgModes)
+	
+	
 		
 		
 def aline(line):
